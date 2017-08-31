17ad435ad8bac97ca969a24cf05ca3f8ab5d5182
Arckman <fengrj1989@gmail.com>
2017-08-31 11:10:05 +0800

    fix bugs,add parameter requests

--- AD_scripts/brute_dir/brute_dir.py
+++ AD_scripts/brute_dir/brute_dir.py
@@ -2,25 +2,40 @@
 import requests
 import time
 import logging
+import itertools
+import base64
+import urllib
 
 input_Filename_target = "target.txt"
 ports = ['80']
 input_Filename_dic = "PHP.txt"
 interval = 0.1  # 延迟(秒)
+mode='development'
+#mode='product'
+'''
+construct parameters,misleading network forensic
+'''
+params=['curl','system','cat']	#some focusing cmd during forensic
+values=[r'http://','/home/web/flag/flag']	#flag file path,flag commit url
 
 target = []
 dic = []
 
-mode='development'
-#mode='product'
 if mode=='development':
 	loglevel=logging.DEBUG
 	log_format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
-elif mode=='product'
+elif mode=='product':
 	loglevel=logging.INFO
 	log_format='%(asctime)s %(message)s'
 
 logging.basicConfig(level=loglevel,format=log_format,datefmt='%a, %d %b %Y %H:%M:%S')
+
+all_params=['']
+for p in params:
+	for v in values:
+		all_params.append("?%s=%s"%(p,v))
+		#all_params.append("?%s=%s"%(p,base64.b64encode(v)))
+		#all_params.append("?%s=%s"%(p,urllib.quote(v)))
 
 with open(input_Filename_target,'r') as input_File_target:
 	target = input_File_target.readlines()
@@ -32,12 +47,13 @@ while(True):
 	for path in dic:
 		for port in ports:
 			port='' if port=='80' else ':'+port
-			for ip in target:
-				try:
-					url = "http://%s%s%s"%(ip.strip(),port,path.strip())
-					logging.debug('Accessing %s'%(url,))
-					requests.get(url,timeout=1)			
-				except:
-					logging.info("Unable connect to URL: http://%s%s"%(ip.strip(),port))
+			for p in all_params:
+				for ip in target:
+					try:
+						url = "http://%s%s%s%s"%(ip.strip(),port,path.strip(),p)
+						logging.debug('Accessing %s'%(url,))
+						requests.get(url,timeout=1)			
+					except:
+						logging.info("Unable connect to URL: http://%s%s"%(ip.strip(),port))
 			time.sleep(interval)
 
